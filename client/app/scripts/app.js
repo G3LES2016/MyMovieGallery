84d20f7bcca1cfd4b4e646ee9ab8c85d95587b34
rafatutankamon <rafa.quevedo@yahoo.com.br>
2016-05-09 21:10:37 -0300

    Conexao Client-Server
    puxando os dados cadastrados no mongo

---
+++ client/app/scripts/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngResource',
+    'ngRoute',
+    'restangular'
+  ])
+  .config(function ($routeProvider,RestangularProvider) {
+    RestangularProvider.setBaseUrl('http://localhost:3000');
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/movies', {
+        templateUrl: 'views/movies.html',
+        controller: 'MoviesCtrl',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .factory('MovieRestangular', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+      RestangularConfigurer.setRestangularFields({
+        id: '_id'
+      });
+    });
+  })
+  .factory('Movie', function(MovieRestangular) {
+    return MovieRestangular.service('movie');
+  })
