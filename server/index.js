8cd0d7b1d711e8e0c695d2a338c03e8d14c35443
rafatutankamon <rafa.quevedo@yahoo.com.br>
2016-05-31 19:18:22 -0300

    testes para mudança da view

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var favicon = require('serve-favicon');
 var _ = require('lodash');
 
 //criando a aplicação
@@ -11,8 +12,7 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
-
-// CORS Support
+app.use(favicon('../client/app/favicon.ico'));// CORS Support
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
